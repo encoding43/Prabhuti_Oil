78eed08dde0b97535ad93cb1f57c88a0b441bc72
encoding43 <encoding43@gmail.com>
2025-03-20 18:00:12 +0530

    Updated Pathname to always as string

--- src/@menu/components/vertical-menu/MenuItem.tsx
+++ src/@menu/components/vertical-menu/MenuItem.tsx
@@ -71,7 +71,7 @@ const MenuItem: ForwardRefRenderFunction<HTMLLIElement, MenuItemProps> = (props,
   const [active, setActive] = useState(false)
 
   // Hooks
-  const pathname = usePathname()
+  const pathname = usePathname() || ''
   const { menuItemStyles, renderExpandedMenuItemIcon, textTruncate } = useVerticalMenu()
 
   const { toggleVerticalNav, isToggled, isBreakpointReached } = useVerticalNav()
